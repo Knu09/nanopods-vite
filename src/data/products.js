9a29e54950ebb59c1964be9d452720c2f103eab6
Knu09 <christiandelatorre1985@gmail.com>
2024-06-29 18:44:13 +0000

    Add database for products

---
+++ src/data/products.js
@@ -0,0 +1,41 @@
+export const PRODUCTS = [
+    {
+        name: 'AirPods',
+        gen: '3rd Generation',
+        price: '10,990',
+        feature: [
+            'Personalized Spatial Audio',
+            'Sweate and water resistant',
+            'Lightning Charging Case',
+            'Up to 6 hours of listening time',
+        ],
+        isCurrent: true,
+    },
+    {
+        name: 'AirPods Pro',
+        gen: '2nd Generation',
+        price: '14,990',
+        feature: [
+            'Personalized Spatial Audio',
+            'Dust, Sweaut and water resistant',
+            'MagSafe Charging Case',
+            'Up to 6 hours of listening time',
+            'Active Noise Cancellation and Transparency mode',
+            'Converstaion Awareness',
+        ],
+        isCurrent: false,
+    },
+    {
+        name: 'AirPods Max',
+        gen: '',
+        price: '32,990',
+        feature: [
+            'Personalized Spatial Audio',
+            'Smart Case',
+            'Up to 20 hours of listening time',
+            'Adaptive Audio',
+            'Active Noise Cancellation and Transparency mode',
+        ],
+        isCurrent: false,
+    },
+]
