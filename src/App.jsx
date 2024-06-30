4e06c5f315ab7a70e9f8d1b4da660e93585fa504
Knu09 <christiandelatorre1985@gmail.com>
2024-06-30 13:43:23 +0000

    Add Footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import Hero from "./components/Hero.jsx";
-import Navbar from "./components/Navbar.jsx";
-import Features from "./components/Features.jsx";
-import { GuideSection } from "./components/GuideSection.jsx";
-import { Product } from "./components/Product.jsx";
+import Hero from './components/Hero.jsx'
+import Navbar from './components/Navbar.jsx'
+import Features from './components/Features.jsx'
+import { GuideSection } from './components/GuideSection.jsx'
+import { Product } from './components/Product.jsx'
+import Contact from './components/Contact.jsx'
+import Footer from './components/Footer.jsx'
 function App() {
-  return (
-    <>
-      <Navbar />
-      <div className="App max-sm:text-center bg-[#ddd] font-paragraph">
-        <Hero />
-        <Features />
-        <GuideSection />
-        <Product />
-      </div>
-      <li></li>
-      <li></li>
-    </>
-  );
+    return (
+        <>
+            <Navbar />
+            <div className="App max-sm:text-center bg-[#ddd] font-paragraph">
+                <Hero />
+                <Features />
+                <GuideSection />
+                <Product />
+                <Contact />
+                <Footer />
+            </div>
+        </>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import img1 from '../assets/apple.png'
+
+const Footer = () => {
+    return (
+        <div className="container max-w-full py-10 text-white bg-black">
+            <div className="flex flex-row px-12 items-center justify-between mx-auto max-w-[1460px]">
+                <div>
+                    <a
+                        target="_blank"
+                        href="https://knu09.github.io/v2.christiandelatorre/"
+                    >
+                        <p>@ Copyright 2024</p>
+                        <p>Made by Christian De la Torre</p>
+                    </a>
+                </div>
+                <a
+                    target="_blank"
+                    href="https://www.apple.com/ph/airpods/"
+                    className="flex items-center gap-x-3"
+                >
+                    <img src={img1} alt="Apple" />
+                    <p>Philippines</p>
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
